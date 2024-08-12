00149b8eabf4053c5623b8cd52d41dd8ac897979
mareduard <159054835+mareduard@users.noreply.github.com>
2024-08-12 13:54:19 -0300

    Add files via upload

---
+++ dump.sqlite.sql
@@ -0,0 +1,29 @@
+-- TABLE
+CREATE TABLE estudantes(
+  nome varchar (200),
+  idade int (2)
+  );
+CREATE TABLE funcionarios(
+  nome varchar (255),
+  CPF varchar (11),
+  salario int (5),
+  setor varchar (8),
+  contratacao date (8)
+  );
+CREATE TABLE livro(
+  nome varchar (30),
+  editora varchar (30)
+  );
+CREATE TABLE produtos(
+  descricao varchar(50),
+  preco varchar(20),
+  quantidade varchar(10),
+  total varchar(5)
+  );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
